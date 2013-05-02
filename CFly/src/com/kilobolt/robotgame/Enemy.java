9020a58bacb7be3a732c1c42c8b091ef256e7bb9
WU YONGSHOU <footballlifer@hotmail.com>
2013-05-02 23:02:18 +0900

    enemy class change

--- CFly/src/com/kilobolt/robotgame/Enemy.java
+++ CFly/src/com/kilobolt/robotgame/Enemy.java
@@ -2,11 +2,14 @@ package com.kilobolt.robotgame;
 
 import android.graphics.Rect;
 
-
 public class Enemy {
 
-	private int power, centerX, speedX, centerY;
-	//private Background bg = GameScreen.getBg1();
+	private int centerX;
+	private int centerY;
+	private int speedY;
+	private int power;
+
+	private Background bg = GameScreen.getBg1();
 	private Robot robot = GameScreen.getRobot();
 
 	public Rect r = new Rect(0, 0, 0, 0);
@@ -17,37 +20,35 @@ public class Enemy {
 	// Behavioral Methods
 	public void update() {
 		follow();
-		centerX += speedX;
-	//	speedX = bg.getSpeedX() * 5 + movementSpeed;
+		centerY += speedY;
+		speedY = bg.getSpeedX() * 5 + movementSpeed;
 		r.set(centerX - 25, centerY - 25, centerX + 25, centerY + 35);
 
 		if (Rect.intersects(r, Robot.yellowRed)) {
 			checkCollision();
 		}
-		
 
 	}
 
 	private void checkCollision() {
 		if (Rect.intersects(r, Robot.rect)|| Rect.intersects(r, Robot.rect2)
 				|| Rect.intersects(r, Robot.rect3) || Rect.intersects(r, Robot.rect4)) {
-
 		}
 	}
 
 	public void follow() {
 		
-		if (centerX < -95 || centerX > 810){
+		if (centerY < -95 || centerY > 810){
 			movementSpeed = 0;
 		}
 
-		else if (Math.abs(robot.getCenterX() - centerX) < 5) {
+		else if (Math.abs(robot.getCenterY() - centerY) < 5) {
 			movementSpeed = 0;
 		}
 
 		else {
 
-			if (robot.getCenterX() >= centerX) {
+			if (robot.getCenterY() >= centerY) {
 				movementSpeed = 1;
 			} else {
 				movementSpeed = -1;
@@ -68,8 +69,8 @@ public class Enemy {
 		return power;
 	}
 
-	public int getSpeedX() {
-		return speedX;
+	public int getSpeedY() {
+		return speedY;
 	}
 
 	public int getCenterX() {
@@ -80,16 +81,16 @@ public class Enemy {
 		return centerY;
 	}
 
-//	public Background getBg() {
-//		return bg;
-//	}
+	public Background getBg() {
+		return bg;
+	}
 
 	public void setPower(int power) {
 		this.power = power;
 	}
 
-	public void setSpeedX(int speedX) {
-		this.speedX = speedX;
+	public void setSpeedX(int speedY) {
+		this.speedY = speedY;
 	}
 
 	public void setCenterX(int centerX) {
@@ -100,8 +101,8 @@ public class Enemy {
 		this.centerY = centerY;
 	}
 
-//	public void setBg(Background bg) {
-//		this.bg = bg;
-//	}
+	public void setBg(Background bg) {
+		this.bg = bg;
+	}
 
 }
